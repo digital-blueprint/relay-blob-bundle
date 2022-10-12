615f54179d8fde72a56cb1f1c94095453c5000d8
Tamara Steinwender <tamara.steinwender@tugraz.at>
2022-10-12 10:45:14 +0200

    Make linter happy

--- src/Service/BlobService.php
+++ src/Service/BlobService.php
@@ -8,10 +8,10 @@ use Dbp\Relay\BlobBundle\Entity\FileData;
 use Dbp\Relay\CoreBundle\Exception\ApiError;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Uid\Uuid;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class BlobService
 {
