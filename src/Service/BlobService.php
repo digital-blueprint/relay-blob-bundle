a431bf477f796281db8beca861c266943a68ad00
Tamara Steinwender <tamara.steinwender@tugraz.at>
2022-10-12 10:44:08 +0200

    Make linter happy

--- src/Service/BlobService.php
+++ src/Service/BlobService.php
@@ -11,6 +11,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class BlobService
 {
