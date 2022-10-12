b3fd01094218b1738219f040356a44c84e13cbde
Tamara Steinwender <tamara.steinwender@tugraz.at>
2022-10-12 16:53:48 +0200

    Change inital migration, remove comment for datetimeimutable

--- src/Migrations/Version20221011110238.php
+++ src/Migrations/Version20221011110238.php
@@ -18,7 +18,7 @@ final class Version20221011110238 extends EntityManagerMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE blob_files (identifier VARCHAR(50) NOT NULL, prefix VARCHAR(255) NOT NULL, file_name VARCHAR(50) NOT NULL, bucket_id VARCHAR(50) NOT NULL, date_created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', last_access DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', retention_duration VARCHAR(50) NOT NULL, idle_retention_duration VARCHAR(50) NOT NULL, content_url VARCHAR(255) NOT NULL, additional_metadata LONGTEXT NOT NULL, PRIMARY KEY(identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE blob_files (identifier VARCHAR(50) NOT NULL, prefix VARCHAR(255) NOT NULL, file_name VARCHAR(50) NOT NULL, bucket_id VARCHAR(50) NOT NULL, date_created DATETIME NOT NULL, last_access DATETIME NOT NULL, retention_duration VARCHAR(50) NOT NULL, idle_retention_duration VARCHAR(50) NOT NULL, content_url VARCHAR(255) NOT NULL, additional_metadata LONGTEXT NOT NULL, PRIMARY KEY(identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
