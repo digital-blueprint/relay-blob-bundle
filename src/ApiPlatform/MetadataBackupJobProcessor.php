3aba7896c1d0b2a6dd3dcdaf23498d96ead01c1a
Manuel Kocher <manuel.kocher@tugraz.at>
2025-03-19 15:57:06 +0100

    Fix linter issues

--- src/ApiPlatform/MetadataBackupJobProcessor.php
+++ src/ApiPlatform/MetadataBackupJobProcessor.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BlobBundle\ApiPlatform;
 
-use Dbp\Relay\BlobBundle\Configuration\ConfigurationService;
 use Dbp\Relay\BlobBundle\Entity\MetadataBackupJob;
 use Dbp\Relay\BlobBundle\Service\BlobService;
 use Dbp\Relay\BlobBundle\Service\DatasystemProviderService;
