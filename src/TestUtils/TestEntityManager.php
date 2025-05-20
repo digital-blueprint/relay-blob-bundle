259e22f480f88b79ae43c7c3722033f95a065d89
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2025-05-20 13:29:49 +0200

    release

--- src/TestUtils/TestEntityManager.php
+++ src/TestUtils/TestEntityManager.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BlobBundle\TestUtils;
 
+use Dbp\Relay\BlobBundle\DependencyInjection\DbpRelayBlobExtension;
 use Dbp\Relay\BlobBundle\Entity\FileData;
+use Dbp\Relay\CoreBundle\TestUtils\TestEntityManager as CoreTestEntityManager;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class TestEntityManager extends \Dbp\Relay\CoreBundle\TestUtils\TestEntityManager
+class TestEntityManager extends CoreTestEntityManager
 {
     public function __construct(ContainerInterface $container)
     {
-        parent::__construct($container, 'dbp_relay_blob_bundle', FileData::class);
+        parent::__construct($container, DbpRelayBlobExtension::ENTITY_MANAGER_ID, FileData::class);
+    }
+
+    public static function setUpBlobEntityManager(ContainerInterface $container): EntityManager
+    {
+        return self::setUpEntityManager($container, DbpRelayBlobExtension::ENTITY_MANAGER_ID);
     }
 
     public function getFileDataById(string $identifier): ?FileData
